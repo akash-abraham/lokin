0e8ed478bf3e6a322f76da925a918e4bf76bc593
sreehhari <sreehari9300@gmail.com>
2024-10-05 20:06:38 +0530

    initialized prisma

---
+++ prisma/migrations/20241005143557_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "points" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
